b770697ba2268c7984acb69255ce2f9901895472
azzaz <azzaz@azzazs-Mac.local>
2018-04-30 00:32:04 +0200

    First Uploading

--- fact_1/ViewController.swift
+++ fact_1/ViewController.swift
@@ -8,8 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
 
+    @IBOutlet weak var textFieldEnterNum: UITextField!
+    
+    @IBOutlet weak var label2ShowFact: UILabel!
+    
+    @IBAction func buttonComputeFact(_ sender: Any) {
+
+        textFieldEnterNum.resignFirstResponder()
+
+        
+        var n : Int = 0
+        var fact = 1
+        let num = NSCharacterSet.decimalDigits.inverted
+
+        
+        if !(textFieldEnterNum.text?.isEmpty)! &&
+            textFieldEnterNum.text?.rangeOfCharacter(from: num) == nil
+        {
+            
+            n = (Int)(textFieldEnterNum.text!)!
+            
+            for _ in 1...n
+            {
+                fact *= n
+                n -= 1
+                let castFact : String = (String)(fact)
+            label2ShowFact .text = castFact
+     
+            }
+            
+        }
+        else
+        {
+            label2ShowFact.text = "plz enter number in text  field"
+        }
+        
+       
+    }
+    
+    
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
